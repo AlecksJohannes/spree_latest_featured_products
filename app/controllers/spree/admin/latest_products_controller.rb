87935ed1dc44923c23134d9382711eea260090b9
AlecksJohannes <alecksjohanssen@gmail.com>
2020-03-06 11:13:02 +0700

    Add default per page in latest_products controller

--- app/controllers/spree/admin/latest_products_controller.rb
+++ app/controllers/spree/admin/latest_products_controller.rb
@@ -1,17 +1,19 @@
 module Spree
-	module Admin
-		class LatestProductsController < ResourceController
-			def index
-				@latest_products = collection
-			end
+  module Admin
+    class LatestProductsController < ResourceController
+      PER_PAGE = 25
 
-			private
+      def index
+        @latest_products = collection
+      end
 
-			def collection
-				params[:q] ||= {}
-				@search = Spree::LatestProduct.ransack(params[:q])
-				@collection = @search.result.includes([:product]).page(params[:page]).per(params[:per_page])
-			end
-		end
-	end
+      private
+
+      def collection
+        params[:q] ||= {}
+        @search = Spree::LatestProduct.ransack(params[:q])
+        @collection = @search.result.includes([:product]).page(params[:page]).per(PER_PAGE)
+      end
+    end
+  end
 end
